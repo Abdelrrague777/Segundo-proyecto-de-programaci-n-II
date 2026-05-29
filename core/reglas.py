46744bef3cd96bb20dc1016e60b23bf32f4c49b8
Abdelrrague777 <catarinoviastrea@gmail.com>
2026-05-29 14:19:43 -0600

    Agrege las reglas del juego dandole los valores numericos a los nombres de los valores como tambien hice la clase regla, que contiene los instruciones para validar cada accion como mover una acrta a una columna si su valor es = K si se puede mver a otra posision no columna si su valor es uno menor a la carta destino,y si jugador gano que sedetermina si se cuenta 13 cartas en cada base

---
+++ core/reglas.py
@@ -0,0 +1,42 @@
+VALORES_CARTAS = {
+    "A": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+}
+
+class ReglasJuego:
+
+    @staticmethod
+    def puede_mover_a_columna(carta, carta_destino):
+        if carta_destino is None:
+            return carta.valor == "K"
+
+        return (
+            carta.color != carta_destino.color
+            and VALORES_CARTAS[carta.valor]
+            == VALORES_CARTAS[carta_destino.valor] - 1
+        )
+
+    @staticmethod
+    def puede_mover_a_base(carta, carta_destino):
+        if carta_destino is None:
+            return carta.valor == "A"
+        return (
+            carta.palo == carta_destino.palo
+            and VALORES_CARTAS[carta.valor]
+            == VALORES_CARTAS[carta_destino.valor] + 1
+        )
+
+    @staticmethod
+    def juego_ganado(bases):
+        return all(len(base.cartas) == 13 for base in bases)
